d449a1f33699dbd718b475f5d235ed1fac0a78c8
EdisonM12 <eddy.matute2004@gmail.com>
2025-11-14 17:31:29 -0500

    migraciones completas

--- Proyecto/app/models.py
+++ Proyecto/app/models.py
@@ -44,3 +44,29 @@ class Calificacion(models.Model):
         return f"{self.estudiante} - {self.evaluacion} = {self.nota}"
 
 
+
+class Profesor(models.Model):
+    nombre= models.CharField(max_length=100)
+    apellido= models.CharField(max_length=100)
+    cedula= models.CharField(max_length=100, unique=True)
+    correo= models.EmailField(max_length=100, unique=True)
+    fecha_nacimiento= models.DateField()
+    materia= models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.nombre
+
+
+class Curso(models.Model):
+    codigo= models.CharField(max_length=10, unique=True)
+    nombre = models.CharField(max_length=120)
+    descripcion = models.CharField(max_length=120)
+    profesor = models.ForeignKey(
+        Profesor,
+        on_delete=models.CASCADE,
+        related_name="cursos"
+    )
+
+
+    def _str_(self):
+        return f"{self.codigo} - {self.nombre}"
